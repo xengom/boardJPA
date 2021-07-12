7172fe1443da06fa60866901b829297b10a4a743
DeC <67894454+xengom@users.noreply.github.com>
2021-07-12 11:10:57 +0900

    gitignore

--- src/main/java/com/xengom/web/IndexController.java
+++ src/main/java/com/xengom/web/IndexController.java
@@ -1,12 +1,20 @@
 package com.xengom.web;
 
+import com.xengom.service.posts.PostsService;
+import com.xengom.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
@@ -14,4 +22,13 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+
+
 }
