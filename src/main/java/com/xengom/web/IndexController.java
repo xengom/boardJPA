d35b472f92ad4c468278967f509f0464ebab3153
DeC <67894454+xengom@users.noreply.github.com>
2021-07-13 18:31:00 +0900

    210713

--- src/main/java/com/xengom/web/IndexController.java
+++ src/main/java/com/xengom/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.xengom.web;
 
+import com.xengom.config.auth.LoginUser;
+import com.xengom.config.auth.dto.SessionUser;
 import com.xengom.service.posts.PostsService;
 import com.xengom.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,13 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user!=null){
+            model.addAttribute("apiUser", user.getName());
+        }
         return "index";
     }
 
